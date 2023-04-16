1073634f15a395ca160cb02f9a5dbf15f688bb8c
Myos <myos.chen@gmail.com>
2023-04-16 17:50:05 +0800

    :lipstick: change where tooltips appear

--- components/tooltip.tsx
+++ components/tooltip.tsx
@@ -14,6 +14,7 @@ function Tooltip({ children, text }: Props) {
         <TooltipPrimitive.Portal>
           <TooltipPrimitive.Content
             className="font-paragraph data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade select-none rounded-[4px] bg-neutral-800 px-4 py-2 text-sm font-medium leading-none text-white will-change-[transform,opacity] dark:bg-slate-200 dark:text-black"
+            side="bottom"
             sideOffset={5}
           >
             {text}
