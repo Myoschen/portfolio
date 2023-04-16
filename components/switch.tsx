df407b4019356fa08625911410b2548f7a8618d8
Myos <myos.chen@gmail.com>
2023-04-17 01:18:17 +0800

    :lipstick: change switch background color

--- components/switch.tsx
+++ components/switch.tsx
@@ -24,9 +24,9 @@ function Switch({ id, leftIcon, rightIcon, checked, onCheckedChange }: Props) {
         id={id}
         checked={checked}
         onCheckedChange={onCheckedChange}
-        className="relative h-[25px] w-[50px] rounded-full bg-gray-200 outline-none focus:outline focus:outline-gray-200 dark:bg-gray-500 dark:focus:outline-gray-500"
+        className="bg-light-secondary focus:outline-light-secondary dark:bg-dark-secondary dark:focus:outline-dark-secondary relative h-[25px] w-[50px] rounded-full outline-none focus:outline"
       >
-        <SwitchPrimitive.Thumb className="block h-5 w-5 translate-x-0.5 rounded-full bg-white shadow transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[27px]" />
+        <SwitchPrimitive.Thumb className="bg-light-primary-100 dark:bg-dark-primary-500 block h-5 w-5 translate-x-0.5 rounded-full shadow transition-[background-color_transform] will-change-transform data-[state=checked]:translate-x-[27px]" />
       </SwitchPrimitive.Root>
       {rightIcon ? (
         <label className="leading-none" htmlFor={id}>
