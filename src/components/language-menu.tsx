3802b9c16b60b53ca8490f3de58b0be5809c0860
Myos <myos.chen@gmail.com>
2023-07-03 01:51:50 +0800

    :recycle: Refactor language menu

--- src/components/language-menu.tsx
+++ src/components/language-menu.tsx
@@ -3,32 +3,40 @@
 import {useMemo, useState} from 'react';
 import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
 import {AnimatePresence, motion} from 'framer-motion';
-import {useLocale} from 'next-intl';
+import {useTranslations} from 'next-intl';
 import {usePathname} from 'next-intl/client';
 import Link from 'next-intl/link';
 
 import Icon from '@/components/ui/icon';
 
-function LanguageMenu() {
+export default function LanguageMenu() {
   const [open, setOpen] = useState(false);
-  const pathname = usePathname();
-  const locale = useLocale();
-  const nextLocale = useMemo(
-    () => (locale === 'en' ? 'zh-TW' : 'en'),
-    [locale]
+  const t = useTranslations('menu');
+  const languages = useMemo(
+    () => [
+      {
+        title: t('en'),
+        locale: 'en',
+      },
+      {
+        title: t('zh-tw'),
+        locale: 'zh-TW',
+      },
+    ],
+    [t]
   );
 
   return (
     <DropdownMenuPrimitive.Root open={open} onOpenChange={setOpen}>
       <DropdownMenuPrimitive.Trigger className="inline-flex items-center gap-x-2 font-medium tracking-wide outline-none">
         <Icon name="language" />
-        {nextLocale === 'en' ? '語言' : 'Language'}
+        {t('language')}
       </DropdownMenuPrimitive.Trigger>
       <AnimatePresence>
         {open && (
           <DropdownMenuPrimitive.Portal forceMount>
             <DropdownMenuPrimitive.Content
-              className="rounded bg-mauve-alpha-6 p-1 backdrop-blur dark:bg-mauve-dark-alpha-6"
+              className="rounded bg-mauve-alpha-3 p-1 backdrop-blur dark:bg-mauve-dark-alpha-3"
               align="start"
               asChild
             >
@@ -47,27 +55,10 @@ function LanguageMenu() {
                   },
                 }}
               >
-                <DropdownMenuPrimitive.Item asChild>
-                  <Link
-                    className="relative flex cursor-default select-none items-center justify-center rounded px-2 py-1 text-sm font-medium leading-none tracking-wide outline-none data-[highlighted]:bg-violet-8 dark:data-[highlighted]:bg-violet-dark-8"
-                    href={pathname}
-                    locale="zh-TW"
-                    replace={true}
-                  >
-                    繁體中文
-                  </Link>
-                </DropdownMenuPrimitive.Item>
-                <DropdownMenuPrimitive.Item asChild>
-                  <Link
-                    className="relative flex cursor-default select-none items-center justify-center rounded px-2 py-1 text-sm font-medium leading-none tracking-wide outline-none data-[highlighted]:bg-violet-8 dark:data-[highlighted]:bg-violet-dark-8"
-                    href={pathname}
-                    locale="en"
-                    replace={true}
-                  >
-                    English
-                  </Link>
-                </DropdownMenuPrimitive.Item>
-                <DropdownMenuPrimitive.Arrow className="fill-mauve-alpha-6 dark:fill-mauve-dark-alpha-6" />
+                {languages.map((lang) => (
+                  <MenuItem key={lang.locale} {...lang} />
+                ))}
+                <DropdownMenuPrimitive.Arrow className="fill-mauve-alpha-3 dark:fill-mauve-dark-alpha-3" />
               </motion.div>
             </DropdownMenuPrimitive.Content>
           </DropdownMenuPrimitive.Portal>
@@ -77,4 +68,24 @@ function LanguageMenu() {
   );
 }
 
-export default LanguageMenu;
+interface MenuItemProps {
+  title: string;
+  locale: string;
+}
+
+function MenuItem({title, locale}: MenuItemProps) {
+  const pathname = usePathname();
+
+  return (
+    <DropdownMenuPrimitive.Item asChild>
+      <Link
+        className="relative flex cursor-default select-none items-center justify-center rounded px-3 py-1 text-sm font-medium leading-none tracking-wide outline-none data-[highlighted]:bg-violet-10 data-[highlighted]:text-mauve-dark-12 dark:data-[highlighted]:bg-violet-dark-10 dark:data-[highlighted]:text-mauve-12"
+        href={pathname}
+        locale={locale}
+        replace={true}
+      >
+        {title}
+      </Link>
+    </DropdownMenuPrimitive.Item>
+  );
+}
