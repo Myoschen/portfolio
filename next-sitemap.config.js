4b6e7fbaccc1b5644bbd9191d76fe83882b4f69c
Myoschen <myos.chen@gmail.com>
2024-08-09 01:09:03 +0800

    chore: clean up all

--- next-sitemap.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://ryan-chen-portfolio.vercel.app/',
-  generateRobotsTxt: true,
-}
--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import createMiddleware from 'next-intl/middleware'
-
-import { defaultLocale, locales } from '@/lib/i18n'
-
-export default createMiddleware({ locales, defaultLocale })
-export const config = {
-  matcher: ['/', '/(en|zh-TW)/:path*', '/((?!_next|_vercel|.*\\..*).*)'],
-}
