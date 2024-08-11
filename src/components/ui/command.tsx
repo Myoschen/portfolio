ed3e9940c8fc0b03f2e76ca4bec89c2863c3a2a4
Myoschen <myos.chen@gmail.com>
2024-08-11 14:42:37 +0800

    fix: missing DialogTitle, DialogDescription

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -5,7 +5,7 @@ import { type DialogProps } from '@radix-ui/react-dialog'
 import { Command as CommandPrimitive } from 'cmdk'
 import { Search } from 'iconoir-react'
 
-import { Dialog, DialogContent } from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/components/ui/dialog'
 import { cn } from '@/lib/utils'
 
 const Command = React.forwardRef<
@@ -28,7 +28,9 @@ interface CommandDialogProps extends DialogProps {}
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0">
+      <DialogTitle className="sr-only">CommandDialog</DialogTitle>
+      <DialogDescription className="sr-only">This is CommandDialog</DialogDescription>
+      <DialogContent className="overflow-hidden p-0" aria-describedby="">
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:size-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:size-5">
           {children}
         </Command>
